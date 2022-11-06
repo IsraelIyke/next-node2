fe8c975f70e84918004dbeac9c8b2992ac2a4c2a
IsraelIyke <85766039+IsraelIyke@users.noreply.github.com>
2022-11-06 10:39:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,45 +10,14 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/results", (req, res) => {
-  const website = req.query.website;
-  const keyword = req.query.keyword;
   puppeteer.use(StealthPlugin());
   (async () => {
     try {
       const browser = await puppeteer.launch({ headless: true });
       const page = await browser.newPage();
 
-      await page.goto("http://search1.epizy.com", { timeout: 0 });
-
-      const grabParagraph = await page.evaluate(() => {
-        const pgTag = document.querySelector("body");
-        return pgTag.innerText;
-      });
-
-      const boolTrueArray = [];
-      const boolFalseArray = [];
-      const lowKeyword = keyword.toLowerCase();
-
-      const lowGrabParagraph = grabParagraph.toLowerCase();
-      const myArray = lowKeyword.split(" ");
-      const newArray = myArray.map((element) => {
-        const bool = lowGrabParagraph.includes(element);
-        if (bool) {
-          boolTrueArray.push(bool);
-        } else {
-          boolFalseArray.push(bool);
-        }
-      });
-      let dbStatus = "";
-      if (boolTrueArray.length > 0 && boolFalseArray.length == 0) {
-        dbStatus = "Perfect";
-      } else if (boolTrueArray.length > 0 && boolFalseArray.length > 0) {
-        dbStatus = "Partial";
-      } else if (boolTrueArray.length == 0 && boolFalseArray.length > 0) {
-        dbStatus = "No";
-      }
-      res.json(dbStatus);
-      //await browser.close();
+      await page.goto("http://search2.epizy.com", { timeout: 0 });
+      await browser.close();
     } catch (e) {
       console.log(`There is a problem here ${e}`);
     }
